201236e3dd36e1cf8ba887d48c766db8d86ba79c
Irfan Shakeel <mirfanshakeel84@gmail.com>
2020-06-14 18:42:15 +1200

    Defined User model, auth middleware and register user functionality

--- routes/users.js
+++ routes/users.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const { check, validationResult } = require('express-validator');
+const User = require('../../models/User');
 
 // @desc    Register user
 // @route   POST api/users
